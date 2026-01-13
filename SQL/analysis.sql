fd24e42bb0578b2987a6868a37ebca26740ab711
AmrSS97 <amrsaadeldin97@gmail.com>
2026-01-13 21:01:18 +0100

    Added analysis.sql file & Spark SQL Runner Script

---
+++ SQL/analysis.sql
@@ -0,0 +1,16 @@
+/** 
+
+This single select statement fullfils all the needed 
+requirnments in the second task of the technical assessment.
+
+Note: COALESCE function used here to avoid breaking calculations if there are NULL values.
+**/
+SELECT
+    SUM(COALESCE(cost_eur, 0)) AS total_maintenance_cost, -- calculating the sum of the maintenance costs for all the events --
+    SUM(COALESCE(downtime_min, 0)) AS total_downtime_minutes, -- calculating the sum of all the downtimes --
+    COUNT(*)                       AS total_maintenance_events, -- calculating the total number of events --
+    SUM(CASE WHEN reason = 'Unplanned Breakdown' THEN 1 ELSE 0 END)
+                                   AS unplanned_breakdowns, -- calculating the total number of unplanned breakdowns --
+    AVG(downtime_min)              AS avg_downtime_per_event -- calculating the average of the downtime per event --
+FROM maintenance_events
+WHERE downtime_min IS NOT NULL;
---
+++ Spark/run_spark_sql.py
@@ -0,0 +1,34 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+#The main function taking csv file path & SQL file path as arguments
+def main(csv_path: str, sql_file: str):
+    spark = (SparkSession.builder
+             .appName("SparkSQL-CSV-Analytics")
+             .getOrCreate())
+
+    # Read CSV(s)
+    df = (spark.read
+          .option("header", "true")
+          .option("inferSchema", "true")
+          .csv(csv_path))
+
+    # Register a temp view for Spark SQL
+    df.createOrReplaceTempView("maintenance_events")
+
+    # Load SQL from file and run it
+    with open(sql_file, "r", encoding="utf-8") as f:
+        query = f.read()
+
+    result = spark.sql(query)
+    result.show(truncate=False)
+    
+    #Stopping the SparkSession
+    spark.stop()
+
+#Validating 3 arguments are passed otherwise output a the prompt passed in the SystemExit method
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise SystemExit("Usage: spark-submit run_spark_sql.py '<csv_path>' '<sql_file>'")
+    main(sys.argv[1], sys.argv[2])
