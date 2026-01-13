e92eb26d48cbe1b7adfd52c660fabfaf6609ea7a
AmrSS97 <amrsaadeldin97@gmail.com>
2026-01-13 23:27:43 +0100

    Added spark-env bat file to Resources folder

---
+++ Resources/spark-env.cmd
@@ -0,0 +1,8 @@
+@echo off
+REM Spark environment (Windows)
+
+set "JAVA_HOME=C:\Program Files\Java\jdk-1.8"
+set "HADOOP_HOME=C:\hadoop"
+set "SPARK_LOCAL_IP=127.0.0.1"
+set "PYSPARK_PYTHON=python"
+set "PYSPARK_DRIVER_PYTHON=python"
